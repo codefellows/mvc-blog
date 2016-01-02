17d14322e29a9ad9557fb8b70806aa336dfd28d2
Brook Riggio <brook@rigg.io>
2016-01-02 00:21:35 -0800

    Simplified basic static blog.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Static MVC Blog
+
+This is a simple blogging system front end. The code is organized with MVC principles in mind.
+
+It starts small, but will be gaining new features regularly. 
---
+++ scripts/article.js
@@ -0,0 +1,44 @@
+var articles = [];
+
+function Article (opts) {
+  // shortcut to show off if requested - show on a later date
+  // Object.keys(opts).forEach(function(e, index, keys) {
+  //   this[e] = opts[e];
+  // },this);
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.removeClass('template');
+  if (!this.publishedOn) {
+    $newArticle.addClass('draft');
+  }
+  $newArticle.data('category', this.category);
+  $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('h1:first').html(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn)
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newArticle.append('<hr>');
+  return $newArticle;
+}
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  articles.push(new Article(ele));
+})
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml())
+});
