418168585fdf1196f34583b200dc38ca8c5d7c5f
Brook Riggio <brook@rigg.io>
2016-01-06 10:45:15 -0800

    Improve variable names

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -3,22 +3,23 @@ var articleView = {};
 
 articleView.populateFilters = function() {
   $('article').each(function() {
+    var authorName, category, optionTag;
     if (!$(this).hasClass('template')) {
       // DONE: We need to take every author name from the page, and make it an option in the Author filter.
       //       To do so, Build an `option` DOM element that we can append to the author select box.
       //       Start by grabbing the author's name from `this` article element, and then use that bit of
       //       text to create the option tag (in a variable named `optionTag`),
       //       that we can append to the #author-filter select element.
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      authorName = $(this).find('address a').text();
+      optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
       $('#author-filter').append(optionTag);
 
       // DONE: Similar to the above, but...
       //       Avoid duplicates! We don't want to append the category name if the select
       //       already has this category as an option!
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
+      category = $(this).attr('data-category');
+      optionTag = '<option value="' + category + '">' + category + '</option>';
+      if ($('#category-filter option[value="' + category + '"]').length === 0) {
         $('#category-filter').append(optionTag);
       }
     }
