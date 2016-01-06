771aef53a5075385589f6a186de6271bfc66e404
Brook Riggio <brook@rigg.io>
2016-01-05 23:51:08 -0800

    Remove previous TODOs.

--- scripts/article.js
+++ scripts/article.js
@@ -16,8 +16,6 @@ Article.prototype.toHtml = function() {
     $newArticle.addClass('draft');
   }
   $newArticle.attr('data-category', this.category);
-  // TODO: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
-  //       Doing so will allow us to use selectors to target articles, based on who wrote them.
   $newArticle.attr('data-author', this.author)
 
   $newArticle.find('.byline a').html(this.author);
