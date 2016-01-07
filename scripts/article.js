4c0e36fc7e57e9dbc852f8201309fea0b1ec436f
Brook Riggio <brook@rigg.io>
2016-01-07 01:33:46 -0800

    Update for class-04:
    Remove class-03 TODOs.
    Convert to Handlebars template.
    Add TODO items for assignment.

--- scripts/article.js
+++ scripts/article.js
@@ -10,24 +10,21 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.removeClass('template');
-  if (!this.publishedOn) {
-    $newArticle.addClass('draft');
-  }
-  $newArticle.attr('data-category', this.category);
-  $newArticle.attr('data-author', this.author)
+  // TODO: Use handlebars to render your articles.
+  //       - Get your template from the DOM.
+  //       - Now "compile" your template with Handlebars.
+  var template = Handlebars.compile($('#article-template').text());
 
-  $newArticle.find('.byline a').html(this.author);
-  $newArticle.find('.byline a').attr('href', this.authorUrl);
-  $newArticle.find('h1:first').html(this.title);
-  $newArticle.find('.article-body').html(this.body);
-  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn)
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-  $newArticle.append('<hr>');
-  return $newArticle;
-}
+  // DONE: If your template will use properties that aren't on the object yet, add them.
+  //   Since your template can't hold any JS logic, we need to execute the logic here.
+  //   The result is added to the object as a new property, which can then be referenced by key in the template.
+  //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+
+  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  return template(this);
+};
 
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
