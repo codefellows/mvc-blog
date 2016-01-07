0cc6e9acb0599fcbf70f0d38f3a9664e37153340
Brook Riggio <brook@rigg.io>
2016-01-07 02:19:12 -0800

    New article form page:
    Markup, styles, and code for new article creation page.
    Live preview supports markdown, and syntax highlighting of backtick-style code blocks.
    Export to JSON, which requires copy/pasting into blogArticles.js to publish.

--- scripts/article.js
+++ scripts/article.js
@@ -10,29 +10,24 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
-  //       - Get your template from the DOM.
-  //       - Now "compile" your template with Handlebars.
   var template = Handlebars.compile($('#article-template').text());
 
-  // DONE: If your template will use properties that aren't on the object yet, add them.
-  //   Since your template can't hold any JS logic, we need to execute the logic here.
-  //   The result is added to the object as a new property, which can then be referenced by key in the template.
-  //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  this.body = marked(this.body);
+  
   return template(this);
 };
 
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+if (typeof rawData !== 'undefined') {
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
 
-rawData.forEach(function(ele) {
-  articles.push(new Article(ele));
-})
+  rawData.forEach(function(ele) {
+    articles.push(new Article(ele));
+  })
+}
 
 articles.forEach(function(a){
   $('#articles').append(a.toHtml())
--- scripts/articleView.js
+++ scripts/articleView.js
@@ -62,10 +62,48 @@ articleView.setTeasers = function() {
   });
 };
 
-$(document).ready(function() {
+articleView.initNewArticlePage = function() {
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('#article-json').on('focus', function(){
+    this.select();
+  });
+
+  $('#new-form').on('change', 'input, textarea', articleView.create);
+};
+
+articleView.create = function() {
+  var article;
+  $('#articles').empty();
+
+  // Instantiate an article based on what's in the form fields:
+  article = new Article({
+    title: $('#article-title').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-url').val(),
+    category: $('#article-category').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#article-published:checked').length ? util.today() : null
+  });
+
+  // Use the Handblebars template to put this new article into the DOM:
+  $('#articles').append(article.toHtml());
+
+  // Activate the highlighting of any code blocks:
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+
+  // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#export-field').show();
+  $('#article-json').val(JSON.stringify(article) + ',');
+};
+
+
+articleView.initIndexPage = function() {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
   articleView.handleMainNav();
   articleView.setTeasers();
-})
+};
