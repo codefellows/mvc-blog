48de500cb014e7a632d2cd99a72132b84429ca30
Brook Riggio <brook@rigg.io>
2016-01-12 23:30:20 -0800

    Add TODOs

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -63,20 +63,27 @@ articleView.setTeasers = function() {
 };
 
 articleView.initNewArticlePage = function() {
+  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later.
   $('.tab-content').show();
+
+  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
   $('#export-field').hide();
   $('#article-json').on('focus', function(){
     this.select();
   });
 
+  // TODO: Add an event handler to update the preview and the export field if any inputs change.
   $('#new-form').on('change', 'input, textarea', articleView.create);
 };
 
 articleView.create = function() {
+  // TODO: Set up a var to hold the new article we are creating.
+  // Clear out the #articles element, so we can put in the updated preview
   var article;
   $('#articles').empty();
 
-  // Instantiate an article based on what's in the form fields:
+  // TODO: Instantiate an article based on what's in the form fields:
   article = new Article({
     title: $('#article-title').val(),
     author: $('#article-author').val(),
@@ -86,15 +93,15 @@ articleView.create = function() {
     publishedOn: $('#article-published:checked').length ? util.today() : null
   });
 
-  // Use the Handblebars template to put this new article into the DOM:
+  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
   $('#articles').append(article.toHtml());
 
-  // Activate the highlighting of any code blocks:
+  // TODO: Activate the highlighting of any code blocks:
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
 
-  // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  // TODO: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
   $('#export-field').show();
   $('#article-json').val(JSON.stringify(article) + ',');
 };
