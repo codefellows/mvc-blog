a4d01955551fbb38e96d2e845b76375280ec93cc
Brook Riggio <brook@rigg.io>
2016-01-18 22:58:13 -0800

    Setup assignment for class-13

--- source/scripts/repo.js
+++ source/scripts/repo.js
@@ -3,18 +3,14 @@
 
   repos.all = [];
 
+  // TODO: Convert this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
+    $.get('/github/users/brookr/repos' +
+          '?per_page=100' +
+          '&sort=updated')
+    .done(function(data, message, xhr) {
+      repos.all = data;
+    });
   };
 
   repos.with = function(attr) {
