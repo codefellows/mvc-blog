54b386ccc0f8379bd401e470b405849621219f9b
Rick Patci <rpatci@gmail.com>
2016-03-22 20:47:59 -0700

    update query string

--- source/scripts/repo.js
+++ source/scripts/repo.js
@@ -6,7 +6,7 @@
   // TODO: Convert this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
     $.get('/github/users/brookr/repos' +
-          '?per_page=100' +
+          '?per_page=5' +
           '&sort=updated')
     .done(function(data, message, xhr) {
       repos.all = data;
