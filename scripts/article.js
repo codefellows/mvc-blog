d40bde944b334a1200a24e2802a6d00a958ba6a0
Rick Patci <rpatci@gmail.com>
2016-05-09 21:46:26 -0700

    update for seattle class-02 jquery and the DOM

--- scripts/article.js
+++ scripts/article.js
@@ -1,10 +1,19 @@
 var articles = [];
 
 function Article (opts) {
-  // shortcut to show off if requested - show on a later date
-  // Object.keys(opts).forEach(function(e, index, keys) {
-  //   this[e] = opts[e];
-  // },this);
+  // TODO: Use the js object passed in to complete this contructor function:
+  // Save ALL the properties of `opts` into `this`.
+
+/* Shortcut to show off if requested - show on a later date
+   Object.keys(opts).forEach(function(e, index, keys) {
+     this[e] = opts[e];
+   },this);
+
+  Alternate "for in" loop:
+    for (keys in opts) {
+      this[keys] = opts[keys];
+    }
+*/
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
   this.title = opts.title;
@@ -15,30 +24,42 @@ function Article (opts) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  // TODO: This cloned article is no longer a template, so we should remove that class...
   $newArticle.removeClass('template');
   if (!this.publishedOn) {
     $newArticle.addClass('draft');
   }
-  $newArticle.data('category', this.category);
+  $newArticle.attr('data-category', this.category);
+  // TODO: Use jQuery to fill in the template with properties
+  // from this particular Article instance. We need to fill in:
+  // the author name and url, the article title and body, and the
+  // publication date.
   $newArticle.find('.byline a').html(this.author);
   $newArticle.find('.byline a').attr('href', this.authorUrl);
   $newArticle.find('h1:first').html(this.title);
   $newArticle.find('.article-body').html(this.body);
+  // This finds time elements with a pubdate attribute of 'datetime':
   $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
+  // This is a separate inclusion of the publication date as a 'title' attribute
+  // to show on hover:
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
 };
 
-rawData.sort(function(a,b) {
+// Sort our data by date published, descending order
+ourLocalData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-rawData.forEach(function(ele) {
+// Now iterate through our transformed collection and instantiate a new Article
+//  instance.
+ourLocalData.forEach(function(ele) {
   articles.push(new Article(ele));
 });
 
+// Append each Article to the DOM
 articles.forEach(function(a) {
   $('#articles').append(a.toHtml());
 });
