fdf9813953b4989fdd86730d7c35203cbc72c21e
Rick Patci <rpatci@gmail.com>
2016-05-11 19:02:15 -0700

    update todo comments class-03 seattle

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -30,7 +30,7 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
-      //       and then show just the ones that match for the author that was selected.
+      //       and then show just the ones that match for the data-author value that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
       $('article').hide();
       $('article[data-author="' + $(this).val() + '"]').fadeIn();
